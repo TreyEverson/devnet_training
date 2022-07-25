8a1e7fb99884d1c9f23e4c883f6f6a4d05cdbb44
Trey Everson <teverson25@me.com>
2022-07-25 13:01:24 -0500

    Containerized Flask API

---
+++ Docker/FlaskAPI.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/endpoint', methods=['GET'])
+
+def get_data():
+    return (jsonify({'message' : 'recieved'}), 200)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
