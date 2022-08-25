72759c8ca724da8def7b5ec3864a48f7a1903994
Trey Everson <teverson25@me.com>
2022-08-24 19:00:56 -0500

    Updated path_trace

--- DNA/path_trace.py
+++ DNA/path_trace.py
@@ -1,4 +1,6 @@
 from email.charset import BASE64
+from http.client import ResponseNotReady
+from importlib.resources import path
 from urllib import response
 import requests
 import json
@@ -16,7 +18,7 @@ response = requests.post(BASE_URL + AUTH_URI, auth=(USERNAME, PASSWORD), verify=
 # print(response['Token'])
 token = response['Token']
 
-# Get Source Device IP
+# Get Source/Destination Device IP
 
 headers = {
     'X-Auth-Token': token,
@@ -24,23 +26,7 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-'''
-headers = {
-    'x-auth-token': token,
-    'User-Agent': "PostmanRuntime/7.16.3",
-    'Accept': "*/*",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "393a9fdd-f109-4fd3-821b-0ae2a03da256,3d2299b4-7515-4f80-9964-b33c2ae9c2ab",
-    'Host': "sandboxdnac2.cisco.com",
-    'Accept-Encoding': "gzip, deflate",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
-}
-'''
-
 DEVICE_URI = '/dna/intent/api/v1/network-device'
-
-queryParams = {}
 
 response = requests.get(BASE_URL + DEVICE_URI, headers=headers, verify=False).json()
 # pprint(response['response'][0])
@@ -48,3 +34,37 @@ source_device = {'hostname': response['response'][0]['hostname'], 'ip': response
 dest_device = {'hostname': response['response'][1]['hostname'], 'ip': response['response'][1]['managementIpAddress']}
 pprint(source_device)
 pprint(dest_device)
+
+# Create Path Trace
+
+PATH_TRACE_URI = '/dna/intent/api/v1/flow-analysis'
+
+path_trace_payload = {
+    'sourceIP': source_device['ip'],
+    'destIP': dest_device['ip'],
+    'inclusions': [
+        'INTERFACE-STATS',
+        'DEVICE-STATS',
+        'ACL-TRACE',
+        'QOS-STATS'
+        ],
+    'protocol': 'icmp'
+}
+
+response = requests.post(f"{BASE_URL}{PATH_TRACE_URI}", headers=headers, json=path_trace_payload, verify=False).json()
+# pprint(response['response'])
+flow_analysis_id = response['response']['flowAnalysisId']
+pprint(flow_analysis_id)
+
+# Get Path Trace Result
+
+response = requests.get(f"{BASE_URL}{PATH_TRACE_URI}/{flow_analysis_id}", headers=headers, verify=False).json()
+pprint(response['response'])
+
+# Get Path Traces
+response = requests.get(f"{BASE_URL}{PATH_TRACE_URI}", headers=headers, verify=False).json()
+pprint(response['response'])
+
+# Delete Path Trace
+response = requests.delete(f'{BASE_URL}{PATH_TRACE_URI}/{flow_analysis_id}', headers=headers, verify=False)
+print(response.status_code)
